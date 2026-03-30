df24e8243675b495d1b03e2bd8662fb143405d32
rayane sekal <rayane.sekal@ynov.com>
2026-03-30 12:06:42 +0200

    Ajout classes PHP et base de données

---
+++ News.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'db.php';
+
+class News {
+    private $pdo;
+
+    public function __construct() {
+        global $pdo;
+        $this->pdo = $pdo;
+    }
+
+    public function getToutesLesNews() {
+        $stmt = $this->pdo->query("SELECT * FROM news ORDER BY date_event ASC");
+        return $stmt->fetchAll();
+    }
+
+    public function ajouterNews($titre, $description, $type, $date_event) {
+        $stmt = $this->pdo->prepare("INSERT INTO news (titre, description, type, date_event) VALUES (?, ?, ?, ?)");
+        return $stmt->execute([$titre, $description, $type, $date_event]);
+    }
+}
+?>
---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "ylink";
+$user = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
